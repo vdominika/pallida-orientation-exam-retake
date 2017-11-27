ef0e7e1893dd25eec38fb86f25c9a864568c15c6
vdominika <vdominika@yahoo.com>
2017-11-27 16:06:11 +0100

    created Warehouse class and HTML

--- clothingproject/src/main/java/com/greenfoxacademy/clothingproject/Controller/MainController.java
+++ clothingproject/src/main/java/com/greenfoxacademy/clothingproject/Controller/MainController.java
@@ -1,9 +1,13 @@
 package com.greenfoxacademy.clothingproject.Controller;
 
+import com.greenfoxacademy.clothingproject.Model.ApiResponse;
+import com.greenfoxacademy.clothingproject.Model.ShoppingPlanner;
+import com.greenfoxacademy.clothingproject.Model.ShoppingPlannerSummary;
 import com.greenfoxacademy.clothingproject.Repository.WarehouseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,10 +16,13 @@ public class MainController {
     @Autowired
     WarehouseRepo repo;
 
-    @GetMapping("/warehouse")
+    /*@GetMapping("/warehouse")
+    return Warehouse;
 
+    @PostMapping("/warehouse/summary")
+    return ShoppingPlannerSummary;
 
-    @
-
+    @GetMapping("/warehouse/query")
+    return ApiResponse;*/
 
 }
