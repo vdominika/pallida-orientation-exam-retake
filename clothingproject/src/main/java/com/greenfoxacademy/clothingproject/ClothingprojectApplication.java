2da64b54f05d786d86345109224f2b2ef792b239
vdominika <vdominika@yahoo.com>
2017-11-27 14:11:08 +0100

    created packages

---
+++ clothingproject/src/main/java/com/greenfoxacademy/clothingproject/ClothingprojectApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.clothingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingprojectApplication.class, args);
+	}
+}
