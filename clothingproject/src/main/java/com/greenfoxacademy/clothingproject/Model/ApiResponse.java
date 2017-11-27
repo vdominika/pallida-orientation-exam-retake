f6a725dd40f09895e37350589425dc625f147168
vdominika <vdominika@yahoo.com>
2017-11-27 15:02:11 +0100

    ApiResponse class done

--- clothingproject/src/main/java/com/greenfoxacademy/clothingproject/Model/ApiResponse.java
+++ clothingproject/src/main/java/com/greenfoxacademy/clothingproject/Model/ApiResponse.java
@@ -1,7 +1,35 @@
 package com.greenfoxacademy.clothingproject.Model;
 
+import java.util.List;
+
 public class ApiResponse {
 
+    String result;
+    List<ShoppingPlanner> clothes;
 
+    public ApiResponse() {
+    }
 
+    public ApiResponse(List<ShoppingPlanner> clothes) {
+        this.result = "ok";
+        this.clothes = clothes;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public List<ShoppingPlanner> getClothes() {
+        return clothes;
+    }
+
+    public void setClothes(List<ShoppingPlanner> clothes) {
+        this.clothes = clothes;
+    }
 }
+
+
